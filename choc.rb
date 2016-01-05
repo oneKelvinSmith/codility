8fb5a6bd11fff098db4df9fdab87d1bf1fb35243
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-01-05 13:18:18 +0200

    Remove exercises

--- choc.rb
+++
@@ -1,9 +0,0 @@
-def gcd(a, b)
-  return b if a % b == 0
-  gcd(b, a % b)
-end
-
-def solution(n, m)
-  lcm = n * m / gcd(n, m)
-  (lcm) / m
-end
--- frog.rb
+++
@@ -1,5 +0,0 @@
-def solution(x, y, d)
-  result = (y - x) / d
-  return result if (y - x) % d == 0
-  result + 1
-end
